534ba14b2600b8b24338ccf38c56a093ffa7ef91
Fehmi Dinçer <fehmi.dincer95@gmail.com>
2025-02-04 21:00:10 +0300

    Sidebar Component is completed

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,10 +1,27 @@
-import { getCategories } from "../Api/Queries/getCategories";
+import type { HomeLoaderResult } from "../Pages/Homepage/homeLoader";
+// import { Link } from "react-router";  for category link
 
-export default function Sidebar() {
-    const response = getCategories();
-    console.log(response);
+export default function Sidebar({data}: {data:HomeLoaderResult}) {
+
+    const renderedCategories = data.allCategories.map( (cat) => {
+        return <div key={cat.categoryName} className="flex flex-col items-center hover:border hover:rounded m-2 p-1">
+            <div>
+                <img src={cat.categoryThumb} alt={cat.categoryName} className="w-24 m-1"/>
+            </div>
+            <div className="text-xl">
+                {cat.categoryName}
+            </div>
+        </div>
+    });
 
     return(
-        <div>Sidebar</div>
+        <div className="border border-gray-100 p-8 bg-green-200">
+            <div className="flex text-2xl font-bold justify-center mb-4">
+                Categories
+            </div>
+            <div className="flex flex-col col-span-2">
+                {renderedCategories}
+            </div>
+        </div>
     );
 }
